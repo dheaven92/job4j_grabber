7eaccde25e04efd3db5e1cba78c0015f09304abc
Daria V <voronovadaria@gmail.com>
2021-10-14 15:24:51 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/grabber/html/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/html/SqlRuParse.java
@@ -9,15 +9,19 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element parent = td.parent();
-            System.out.println(new SqlRuDateTimeParser().parse(parent.children().get(5).text()));
-            System.out.println();
+        int page = 1;
+        while (page < 6) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + page).get();
+            Elements rows = doc.select(".postslisttopic");
+            for (Element td : rows) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element parent = td.parent();
+                System.out.println(new SqlRuDateTimeParser().parse(parent.children().get(5).text()));
+                System.out.println();
+            }
+            page++;
         }
     }
 }
